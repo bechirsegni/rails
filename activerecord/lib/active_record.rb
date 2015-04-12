2cd8e40632e83964f172ad219760e19bfdd3e199
Bechir Segni <bechirsegni@gmail.com>
2015-04-12 15:27:46 +0100

    Update active_record.rb

--- activerecord/lib/active_record.rb
+++ activerecord/lib/active_record.rb
@@ -153,8 +153,7 @@ module ActiveRecord
     autoload :DatabaseTasks
     autoload :SQLiteDatabaseTasks, 'active_record/tasks/sqlite_database_tasks'
     autoload :MySQLDatabaseTasks,  'active_record/tasks/mysql_database_tasks'
-    autoload :PostgreSQLDatabaseTasks,
-      'active_record/tasks/postgresql_database_tasks'
+    autoload :PostgreSQLDatabaseTasks, 'active_record/tasks/postgresql_database_tasks'
   end
 
   autoload :TestFixtures, 'active_record/fixtures'
